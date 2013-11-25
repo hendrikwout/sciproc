26e01597891f50953ec632c600cf660cd0cd2c07
hendrikwout <hendrikwout@gmail.com>
2013-11-25 18:39:22 +0100

    changes 2013-11-25

--- sciproc/sciop.py
+++ sciproc/sciop.py
@@ -1,4 +1,106 @@
 from numpy import zeros,nan
+import numpy as np
+import math as mt
+
+# def getidx(idxs,numfac):
+
+def steinalp(f,numfac):
+    '''  factor seperation procedure following Stein and Alpert (1993), Factor
+    Seperation in Numerical Simulations (JAS). Example will follow.
+
+    f: data set with factors
+    numfac: amount of factors investigated
+
+    f
+    
+    '''
+
+    #a few preparations
+    frav = np.array(f)
+    fshp = frav.shape #np.shape(frav)[:numfac]
+    nmshape = 2**numfac
+    lshf = len(numfac)
+    
+    while nmshape > 1:
+        if frav.shape[0] > 2:
+            frav.shape = frav.shape[0]/2+frac.shape[1:]
+        else:
+            frav.shape = frav.shape[1:]
+        nmshape = nmshape/2
+
+    fsep = np.zeros_like(frav)
+    
+    # here we go...
+    fsep[0] = frav[0][:]
+    for posfsep in range(1,len(fsep)):
+    
+        fsep[posfsep] = frav[posfsep][:]
+        idxs = []
+        posfseptmp = int(posfsep)
+    
+        # we discover the indices from which we take combinations
+        for ishp in reversed(range(lshf)):
+            if np.mod(posfseptmp,2) == 1:
+                idxs.append(ishp)
+                posfseptmp = (posfseptmp - 1)
+            posfseptmp = posfseptmp/2
+    
+        idxs.sort()
+    
+        l = len(idxs)
+    
+        # a loop over the different kind of combinations :
+        # 0 1 2 3...m -- ... l
+        # l l l l   ...      l
+#        print 'idxs', idxs
+        for m in range(1,l):
+            # determine the number of possible combinations of the current kind
+            numcomb = mt.factorial(l)/mt.factorial(m)/mt.factorial(l-m)
+    
+            # print 'm l',m,l
+            # print 'posfsep', posfsep
+            # print 'numcombb',numcomb
+    
+            # now we make the combinations of the indices
+            pos = range(m) # start position (e.g. (1,2,3,4)
+            if pos != []:
+                for inumcomb in range(numcomb):
+    #                print('pos',pos)
+                
+                    curidxs = []
+                    for ipos,epos in enumerate(pos):
+                        # the current matrix index
+                        curidxs.append(idxs[epos])
+    
+   #                  print (curidxs,'curidxs')
+                    posf = 0
+                    for i in range(lshf):
+                        if i in curidxs:
+   #                         print 'i curidxs',i,curidxs,(lshf-i-1)
+                            posf = posf + 2**(lshf-i-1)
+                    
+   #                 print ('posf',posf,np.float((-1)**(l-m) *frav[posf]))
+                    fsep[posfsep] = fsep[posfsep] +  (-1)**(l-m) * frav[posf]
+   #                 print('posone',pos)
+                    pos[-1] = pos[-1] +1 
+                    for ipos,epos in reversed(list(enumerate((pos)))):
+   #                     print ipos,epos,l
+                        if (ipos >= 1) and (epos == l):
+                            pos[ipos - 1] = pos[ipos -1] + 1
+                            pos[ipos] = 0 #pos[ipos] + 1
+#                    print('postwo',pos)
+        fsep[posfsep] = fsep[posfsep] + (-1)**l * frav[0]
+#        print ('lposf',0,np.float(frav[0]))
+        #print 'posfsep fsep',posfsep,fsep[posfsep]
+        
+    fsep.shape = fshp
+    if type(f).__name__ == 'list':
+        return list(fsep)
+    elif type(f).__name__ == 'tuple':
+        return tuple(fsep)
+    else:
+        # just array?
+        return fsep
 
 def tsfill(valin,dtin,dtout):
     # purpose: regrid timeseries... fill missing timesteps in a timeseries with nan values, or leave out unwanted timesteps
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='sciproc',
-    version='0.7.5',
+    version='0.7.8',
     author='H. Wouters',
     author_email='hendrikwout@gmail.com',
     packages=['sciproc'],
